1ff966766e8d678aa2faf625041646d8323cb532
Topan-Roy <roytopan734@gmail.com>
2025-08-11 13:24:51 +0600

    added Study section

--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -2,56 +2,57 @@ import { MdDownload } from "react-icons/md";
 import { CgEyeAlt } from "react-icons/cg";
 import Works from "../../component/Works/Works";
 import Skills from "../../component/Skaill/Skaill";
+import Study from "../../component/Study/Study";
 
 const About = () => {
 
   return (
     <>
-    <section id="about" className="py-10 text-white">
-      <div className="text-center mt-8">
-        <h3 className="text-4xl font-semibold">
-          About <span className="text-cyan-600">Me</span>
-        </h3>
-        <p className="text-gray-400 my-3 text-lg">My introduction</p>
-        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
-          <div className="p-2">
-            <div className="text-gray-300 my-3">
-              <div className="text-justify leading-7 w-11/12 mx-auto">
-              <h1 className="text-4xl mb-1 text-white font-Poppins font-semibold" >Topan Roy </h1>
-              <h4 className="text-cyan-500 mb-2 text-xl"> Front-end Developer</h4>
-              </div>
-              <p className="text-justify leading-7 w-11/12 mx-auto">
-              Allow me to introduce myself: Iam a Junior Development Dynamo! With a passion for blending creativity and coding expertise, I thrive on crafting captivating web experiences that seamlessly merge pixels and logic into user-centric marvels
-              </p>
-              <br />
-              <div className="text-justify leading-7 w-11/12 mx-auto">
-              <p className="text-3xl text-white" > My Resume </p>
-                <div className="flex gap-3 mt-2">
-                  <button className=" text-xl btn btn-outline bg-green-500 ">
-                    <MdDownload></MdDownload>
+      <section id="about" className="py-10 text-white">
+        <div className="text-center mt-8">
+          <h3 className="text-4xl font-semibold">
+            About <span className="text-cyan-600">Me</span>
+          </h3>
+          <p className="text-gray-400 my-3 text-lg">My introduction</p>
+          <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+            <div className="p-2">
+              <div className="text-gray-300 my-3">
+                <div className="text-justify leading-7 w-11/12 mx-auto">
+                  <h1 className="text-4xl mb-1 text-white font-Poppins font-semibold" >Topan Roy </h1>
+                  <h4 className="text-cyan-500 mb-2 text-xl"> Front-end Developer</h4>
+                </div>
+                <p className="text-justify leading-7 w-11/12 mx-auto">
+                  Allow me to introduce myself: Iam a Junior Development Dynamo! With a passion for blending creativity and coding expertise, I thrive on crafting captivating web experiences that seamlessly merge pixels and logic into user-centric marvels
+                </p>
+                <br />
+                <div className="text-justify leading-7 w-11/12 mx-auto">
+                  <p className="text-3xl text-white" > My Resume </p>
+                  <div className="flex gap-3 mt-2">
+                    <button className=" text-xl btn btn-outline bg-green-500 ">
+                      <MdDownload></MdDownload>
                     </button>
                     <button className=" text-xl  btn btn-outline bg-yellow-400 ">
                       <CgEyeAlt></CgEyeAlt>
                     </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
-            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
-              <img
-                src="https://i.ibb.co.com/vxbLPQ4S/IMG-20230528-230617-100.jpg"
-                alt=""
-                className="w-full object-cover bg-cyan-500 rounded-xl"
-              />
+            <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+              <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
+                <img
+                  src="https://i.ibb.co.com/vxbLPQ4S/IMG-20230528-230617-100.jpg"
+                  alt=""
+                  className="w-full object-cover bg-cyan-500 rounded-xl"
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-    <Skills></Skills>
-       <Works></Works>
-   
+      </section>
+      <Skills></Skills>
+      <Works></Works>
+      <Study></Study>
     </>
   );
 };
--- src/component/Works/Works.jsx
+++ src/component/Works/Works.jsx
@@ -5,15 +5,15 @@ import animation from "../../../public/Computer.json"
 const Works = () => {
     const defaultOptions = {
         loop: true,
-        autoplay: true, 
+        autoplay: true,
         animationData: animation,
         rendererSettings: {
-          preserveAspectRatio: 'xMidYMid slice'
+            preserveAspectRatio: 'xMidYMid slice'
         }
-      };
-      const lottieContainerStyle = {
-        transform: 'rotate(0deg)', 
-      };
+    };
+    const lottieContainerStyle = {
+        transform: 'rotate(0deg)',
+    };
 
     const services = [
         {
@@ -30,35 +30,35 @@ const Works = () => {
             id: '03',
             name: 'MERN Stack Developer',
             description: 'Crafting captivating interfaces, and integrating data flawlessly within the MERN stack—your expertise drives high-performance, collaborative, and innovative web experiences from end to end.'
-    }
+        }
     ]
 
     return (
         <div id="work" className=" pt-20 px-10 pb-12  text-white max-w-7xl mx-auto ">
-                <h1 data-aos="fade-up" data-aos-duration="2000" className="text-center uppercase mt-4 lg:mt-12 text-xl font-bold md:text-3xl lg:text-5xl">What I do</h1>
-                <div className="lg:flex justify-evenly lg:gap-16 items-center">
+            <h1 data-aos="fade-up" data-aos-duration="2000" className="text-center uppercase mt-4 lg:mt-12 text-xl font-bold md:text-3xl lg:text-5xl">What I do</h1>
+            <div className="lg:flex justify-evenly lg:gap-16 items-center">
                 <div className="flex-1 ">
-               
+
                     <div className="w-[350px] lg:w-[400px] h-[200px] lg:h-[800px] mt-8 lg-mt-0" style={lottieContainerStyle}>
-                    <Lottie options={defaultOptions} />   
-                        </div>                 
+                        <Lottie options={defaultOptions} />
+                    </div>
+                </div>
+
+                <div data-aos="zoom-in" data-aos-duration="2000" className="flex-1 pl-2 ">
+                    {/* services  */}
+                    {
+                        services.map((item) => (
+                            <div key={item.id} className="card px-3 py-5 mt-8 w-[360px] md:w-[600px] shadow-lg shadow-purple-400">
+                                <h4 className="font-semibold text-2xl text-yellow-600 mb-3">{item.name}</h4>
+                                <p>{item.description}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
 
-            <div data-aos="zoom-in" data-aos-duration="2000" className="flex-1 pl-2 ">
-                {/* services  */}
-                {
-                    services.map((item) => (
-                        <div key={item.id} className="card px-3 py-5 mt-8 w-[360px] md:w-[600px] shadow-lg shadow-purple-400">
-                           <h4 className="font-semibold text-2xl text-yellow-600 mb-3">{item.name}</h4>
-                            <p>{item.description}</p>
-                             </div>
-                    ))
-                }
-            </div>
-                </div>
-         
-         </div>
-   
+        </div>
+
     );
 };
 
